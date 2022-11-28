4c3f9367f04837924a7be841bd0de313880dfa1f
nadunchandrabahu <nadun.c.97@gmail.com>
2022-11-28 11:43:50 +0530

    v.1.1.1

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const showButtons = document.querySelectorAll(".btn");
+const modalItems = document.querySelectorAll(".hidden");
+
+document.querySelector(".btn-close").addEventListener("click", close);
+document.querySelector(".overlay").addEventListener("click", close);
+
+for (let i = 0; i < showButtons.length; i++) {
+  showButtons[i].addEventListener("click", open);
+}
+
+function open() {
+  for (let j = 0; j < modalItems.length; j++) {
+    modalItems[j].style.display = "";
+  }
+}
+
+function close() {
+  for (let i = 0; i < modalItems.length; i++)
+    modalItems[i].style.display = "none";
+}
