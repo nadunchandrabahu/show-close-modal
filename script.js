dfbad6cbc36e0b2d6c1e6c74641906a139b0700b
nadunchandrabahu <nadun.c.97@gmail.com>
2022-11-28 13:41:40 +0530

    v1.1.4
    optimized code

--- script.js
+++ script.js
@@ -3,26 +3,18 @@
 const showButtons = document.querySelectorAll(".btn");
 const popUp = document.querySelectorAll(".hidden");
 const overlay = document.querySelector(".overlay");
-
 const closeButton = document.querySelector(".btn-close");
 
-for (let i = 0; i < showButtons.length; i++) {
+for (let i = 0; i < showButtons.length; i++)
   showButtons[i].addEventListener("click", open);
-}
 
 closeButton.addEventListener("click", close);
 overlay.addEventListener("click", close);
 
 function open() {
-  //
-  for (let i = 0; i < popUp.length; i++) {
-    popUp[i].classList.remove("hidden");
-  }
+  for (let i = 0; i < popUp.length; i++) popUp[i].classList.remove("hidden");
 }
 
 function close() {
-  //
-  for (let i = 0; i < popUp.length; i++) {
-    popUp[i].classList.add("hidden");
-  }
+  for (let i = 0; i < popUp.length; i++) popUp[i].classList.add("hidden");
 }
